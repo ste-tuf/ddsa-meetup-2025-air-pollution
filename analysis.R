7b2f9dc8aa972cacffde683b97915c38c96c3705
Stéphane Tuffier <stephane.tuffier@sund.ku.dk>
2025-04-01 16:59:30 +0200

    Add script

---
+++ analysis.R
@@ -0,0 +1,84 @@
+#' # Dev labs monitors measure summary
+#' 
+library(tidyverse)
+
+data <- read_delim("data/walk.csv", delim = ";")
+
+
+# Clean
+data <- data |> 
+  mutate(
+    `Entity Name` = substr(`Entity Name`, 6, 8) |> 
+      factor()
+  )
+data
+
+data_l <- data |> 
+  pivot_longer(cols = CO2:`T`, names_to = "Measurement", values_to = "Value" ) |> 
+  mutate(Measurement = factor(Measurement,
+                              levels = c( "PM2.5", "NO2", "CO2",  "RH", "T", "Light, data, ALL", 
+                                          "Light_Blue", "Light_Green", "Light_Red", "Light_White",  "Color, Temp."))
+  ) |> 
+  filter(!is.na(Measurement))
+
+#' # Stats
+tab_stat <- data_l |> 
+  group_by(Measurement) |>
+  summarise(
+    mean = mean(Value, na.rm = T),
+    sd = sd(Value, na.rm = T),
+    q1 = quantile(Value, na.rm = T)[2],
+    median = median(Value, na.rm = T),
+    q3 = quantile(Value, na.rm = T)[4],
+    min = min(Value, na.rm = T),
+    max = max(Value, na.rm = T),
+  )
+  
+gt::gt(tab_stat) |> 
+  gt::fmt_number(decimals = 1, drop_trailing_zeros = T)
+
+tab_stat <- tab_stat |> 
+  mutate(Measurement = factor(Measurement,
+                              levels = c( "PM2.5", "NO2", "CO2",  "RH", "T", "Light, data, ALL", 
+                                         "Light_Blue", "Light_Green", "Light_Red", "Light_White",  "Color, Temp."))
+         )
+
+# 
+# data |> 
+#   select(-Timestamp) |> 
+#   gtsummary::tbl_summary(, by = `Entity Name`)
+# 
+#   
+
+#' # Plot
+make_plot <- function(Measurement, data, ...){
+  ggplot(data, aes(x = Timestamp, y = Value, colour = Value, group = `Entity Name`)) +
+    geom_line(linewidth = 1.5) +
+    theme_bw() +
+    labs(title = Measurement) +
+    scale_color_viridis_c(option = "inferno", direction = -1)
+
+}
+
+
+data_nested <- data_l |> 
+  select(-contains("Accele")) |> 
+  group_by(Measurement) |> 
+  nest(data = c(`Entity Name`, Timestamp, Value)) |> 
+  arrange(Measurement)
+
+
+data_nested$plot <- pmap(data_nested, make_plot)
+  
+# # data_nested |> 
+#   # nest(plot, by= `Entity Name`) |> 
+#   pmap(data_nested, 
+#        function(`Entity Name`, plot, ...){
+#          cat(paste("#", `Entity Name`))
+#          plot
+#        }
+#   )
+data_nested$plot
+
+
+# rmarkdown::render("analysis.R")
